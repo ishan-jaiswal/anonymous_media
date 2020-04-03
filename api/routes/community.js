318f8dee0246667ebb773817e8ae380c0326d1d9
ishan-jaiswal <devhjaiswal@gmail.com>
2020-04-03 12:29:47 +0530

    routes/Community.js updated

--- api/routes/community.js
+++ api/routes/community.js
@@ -10,6 +10,55 @@ const Community = require("../models/community");
 
     Promise.all()
   })*/
+  router.get("/",async (req,res)=>{
+    //router.get("/",paginatedResults(Post),(req,res)=>{
+      /*const page=req.query.page
+      const limit=req.query.limit
+      const start=(page-1)*limit
+      const end=(page)*limit
+      const result=Post.slice(start,end)
+      const result = await Post.find().sort('-createdOn');*/
+      //res.json(res.paginatedResults);
+      Community.find()
+        .exec()
+        .then(docs => {
+          console.log(docs);
+          //   if (docs.length >= 0) {
+          res.status(200).json(docs);
+          //   } else {
+          //       res.status(404).json({
+          //           message: 'No entries found'
+          //       });
+          //   }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
+    router.get("/:id",(req,res)=>{
+      const id = req.params.id;
+      Community.findById(id)
+        .exec()
+        .then(doc => {
+          console.log("From database", doc);
+          if (doc) {
+            res.status(200).json(doc);
+          } else {
+            res
+              .status(404)
+              .json({ message: "No valid entry found for provided ID" });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ error: err });
+        });
+    });
+
+
 
 router.post("/new_community",(req,res)=>{
     const community = new Community({
